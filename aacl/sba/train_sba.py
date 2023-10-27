1c57a9e96ff7304d5805e51afaefc22fbb16be2c
Wenyu Huang <wenyuhuang@Wenyus-MBP.broadband>
2023-10-27 19:40:38 +0100

    upload data

--- aacl/sba/train_sba.py
+++ aacl/sba/train_sba.py
@@ -1,4 +1,5 @@
-import argparse
+import datasets
+
 from model import SBA
 from transformers import RobertaConfig, RobertaTokenizer, RobertaModel
 import torch
@@ -6,99 +7,83 @@ import random
 from tqdm import tqdm
 import numpy as np
 
-def load_raw_data(path):
-    raw_data = []
-    with open(path) as f:
-        lines = f.readlines()
-        for line in lines:
-            raw_data.append(line.split('\n')[0])
-    return raw_data
+from transformers import Trainer, TrainingArguments
+from torch.utils.data import Dataset, DataLoader
+import os
+import inspect
 
 
-def load_data(raw_data, batch_size=128, shuffle=False):
-    if shuffle:
-        random.shuffle(raw_data)
-    data = []
-    data_batch = []
-    for data_piece in raw_data:
-        data_batch.append(data_piece)
-        if len(data_batch) == batch_size:
-            data.append(data_batch)
-            data_batch = []
-    if len(data_batch) > 0:
-        data.append(data_batch)
-    return data
+class SBADataset(Dataset):
+    def load_raw_data(self, path):
+        raw_data = []
+        with open(path) as f:
+            lines = f.readlines()
+            for line in lines:
+                raw_data.append(line.split('\n')[0])
+        return raw_data
+
+    def __init__(self, data_dir, split_name):
+        data_path = os.path.join(data_dir, split_name + '.target')
+        self.data = self.load_raw_data(data_path)
+        self.tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        inputs = self.tokenizer(self.data[idx], return_tensors='pt',
+                                padding='max_length', truncation=True, max_length=128)
+        input_ids, attention_mask = inputs['input_ids'], inputs['attention_mask']
+        return {"input_ids": input_ids,
+                "attention_mask": attention_mask}
+
+
+class SBATrainer(Trainer):
+    def compute_loss(self, model, inputs, return_outputs=False):
+        input_ids = inputs['input_ids']
+        attention_mask = inputs['attention_mask']
+        loss = model(input_ids, attention_mask, is_train=True)[1]['loss']
+        return loss
+
+
 
 def main(args):
     random.seed(42)
     torch.manual_seed(42)
 
-    tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
+    train_dataset = SBADataset(args.data_dir, 'train')
+    val_dataset = SBADataset(args.data_dir, 'val')
+    # train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+    # val_dataloader = DataLoader(val_dataset, batch_size=1, shuffle=False)
+
     sba_config = RobertaConfig.from_pretrained('roberta-base')
     sba_config.is_decoder = True
     sba_config.add_cross_attention = True
     sba_config.num_hidden_layers = args.layers
-    sba = SBA(sba_config).cuda()
-    pretrained_LM = RobertaModel.from_pretrained('roberta-base').half().cuda()
+    model = SBA(sba_config)
 
-    optimizer = torch.optim.Adam(sba.parameters(), lr=args.lr)
-    # lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, start_factor=0.1, total_iters=Warmup)
-    for params in sba.named_parameters():
-        print(params[0])
+    training_args = TrainingArguments(
+        output_dir=args.output_dir,  # output directory
+        num_train_epochs=args.epochs,  # total number of training epochs
+        per_device_train_batch_size=args.batch_size,  # batch size per device during training
+        per_device_eval_batch_size=args.batch_size,  # batch size for evaluation
+        warmup_steps=args.warmup,  # number of warmup steps for learning rate scheduler
+        weight_decay=0.01,  # strength of weight decay
+        logging_dir=args.output_dir+'/logs',  # directory for storing logs
+        logging_steps=30,
+        learning_rate=args.lr,
+        save_steps=10000,
+        fp16=True
+    )
 
-    train_data_raw = load_raw_data(args.data_dir + 'train.target')
-    val_data_raw = load_raw_data(args.data_dir + 'val.target')
+    trainer = SBATrainer(model,
+                         args=training_args,
+                         train_dataset=train_dataset,
+                         eval_dataset=val_dataset,
+                         )
 
-    curr_patience = 0
-    curr_best_val_loss = 1e7
-    for epoch in range(args.epochs):
-        sba.train()
-        train_data = load_data(train_data_raw, batch_size=args.batch_size)
-        losses = []
-        for batch in tqdm(train_data, 'Epoch#{}'.format(epoch)):
-            inputs = tokenizer(batch, return_tensors='pt', padding=True, truncation=True)
-            input_ids = inputs['input_ids'].cuda()
-            attention_mask = inputs['attention_mask'].cuda()
-            with torch.no_grad():
-                hidden_states = pretrained_LM(input_ids=input_ids, attention_mask=attention_mask)[0]
-            hidden_states = hidden_states.clone().detach()
-            loss = sba(hidden_states, input_ids, is_train=True)[1]['loss']
-            loss.backward()
-            losses.append(loss.item())
-            optimizer.step()
-            # lr_scheduler.step()
-        print('Finish Epoch#{} with loss {}.'.format(epoch, np.mean(losses)))
-        torch.save(sba.state_dict(), 'ckpts_fp16/last.model')
+    trainer.train()
 
-        val_data = load_data(val_data_raw, batch_size=args.batch_size)
-        losses = []
-
-        with torch.no_grad():
-            for batch in tqdm(val_data, 'Validation: Epoch#{}'.format(epoch)):
-                inputs = tokenizer(batch, return_tensors='pt', padding=True, truncation=True)
-                input_ids = inputs['input_ids'].cuda()
-                attention_mask = inputs['attention_mask'].cuda()
-                hidden_states = pretrained_LM(input_ids=input_ids, attention_mask=attention_mask)[0]
-                loss = sba(hidden_states, input_ids, is_train=True)[1]['loss']
-                losses.append(loss.item())
-            val_loss = np.mean(losses)
-            if val_loss < curr_best_val_loss:
-                print('Epoch#{} validation loss {}, beat previous best loss {}.'.format(
-                    epoch, val_loss, curr_best_val_loss))
-                curr_best_val_loss = val_loss
-                print('Saving as best model to ckpts_fp16/best.model')
-                torch.save(sba.state_dict(), 'ckpts_fp16/best.model')
-                curr_patience = 0
-            else:
-                print('Epoch#{} validation loss {}, did not beat previous best loss {}.'.format(
-                    epoch, val_loss, curr_best_val_loss))
-                curr_patience += 1
-                print('Current early stop patience {}/{}.'.format(curr_patience, args.early_stop_patience))
-                if curr_patience == args.early_stop_patience:
-                    print('Early stop!')
-                    break
-
-    print('Finish training!')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -109,19 +94,24 @@ if __name__ == '__main__':
         default="../ragae/msmarco/"
     )
     parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="./result/"
+    )
+    parser.add_argument(
         "--epochs",
         type=int,
         default=100
     )
     parser.add_argument(
-        "--early_stop_patience",
+        "--warmup",
         type=int,
-        default=15
+        default=1000
     )
     parser.add_argument(
         "--lr",
         type=float,
-        default=1e-5
+        default=3e-5
     )
     parser.add_argument(
         "--layers",
